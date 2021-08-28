2d438fef94fea4b918ae041daf02899054b9f7c6
mtuong79 <89337936+mtuong79@users.noreply.github.com>
2021-08-28 18:35:12 +0700

    Update Covid19_Book.py

--- Covid19_Book.py
+++ Covid19_Book.py
@@ -5,34 +5,34 @@ st.image("img/1022.png")
 
 st.subheader("[ĐƯỜNG DÂY NÓNG](https://covid19.hochiminhcity.gov.vn/duong-day-nong-thanh-pho)")
 xem_yte = st.radio("",
-                   ('Q10-YTe Luu Dong','Q10-SOS','Q10-Yte','Q3-Yte','Q3-SOS'))
-if xem_yte == 'Q10-YTe Luu Dong':
+                   ('Q10-Y Tế Lưu Động','Q10-Tổ phản ứng nhanh F0','Q10-Y Tế','Q3-Y Tế','Q3-Tổ phản ứng nhanh F0'))
+if xem_yte == 'Q10-Y Tế Lưu Động':
     st.image("img/q10-YTeDong.png")
-elif xem_yte == 'Q10-SOS':
+elif xem_yte == 'Q10-Tổ phản ứng nhanh F0':
     st.image("img/q10-p2-SOS.png")
-elif xem_yte == 'Q10-Yte':
+elif xem_yte == 'Q10-Y Tế':
     st.image("img/q10-YTe.jpg")
-elif xem_yte == 'Q3-Yte':
+elif xem_yte == 'Q3-Tổ phản ứng nhanh F0':
     st.image("img/q3-YTe.jpg")
 else:
     st.image("img/q3-p3-SOS.png")
 
-st.subheader("DINH DUONG LA VANG")
-xem_dd = st.radio("",('Dinh Duong','Thuc Pham'))
-if xem_dd == 'Dinh Duong':
+st.subheader("DINH DƯỠNG LÀ VÀNG")
+xem_dd = st.radio("",('Dinh Dưỡng','Thực Phẩm'))
+if xem_dd == 'Dinh Dưỡng':
     st.image("img/STSK-DinhDuong.png")
-elif xem_dd == 'Thuc Pham':
+elif xem_dd == 'Thực Phẩm':
     st.image("img/STSK-ThucPham.png")
 
-st.subheader("Trieu Chung va Dieu Tri Covid")
-xem_covid = st.radio("", ('Trieu Chung','Dieu Tri F0','Cap Cuu'))
-if xem_covid == 'Trieu Chung':
+st.subheader("Triệu Chứng và Điều Trị")
+xem_covid = st.radio("", ('Triệu Chứng','Điều Trị F0','Cấp Cứu'))
+if xem_covid == 'Triệu Chứng':
     st.image("img/STSK-TrieuChung.png")
-elif xem_covid == 'Dieu Tri F0':
+elif xem_covid == 'Điều Trị F0':
     st.image("img/STSK-F0.png")
 else:
     st.image("img/STSK-CapCuu.png")
 
-st.subheader("TIM HIEU THEM")
+st.subheader("TÌM HIỂU THÊM")
 st.write("Cong thong tin Covid19 [HCM](https://covid19.hochiminhcity.gov.vn/home)")
 st.write("[Sổ tay](https://covid19.hochiminhcity.gov.vn/cham-soc-suc-khoe/-/asset_publisher/ZSHLi888uq2s/content/so-tay-suc-khoe-covid--1?inheritRedirect=false&redirect=https%3A%2F%2Fcovid19.hochiminhcity.gov.vn%2Fcham-soc-suc-khoe%3Fp_p_id%3D101_INSTANCE_ZSHLi888uq2s%26p_p_lifecycle%3D0%26p_p_state%3Dnormal%26p_p_mode%3Dview%26p_p_col_id%3Dcolumn-10%26p_p_col_count%3D1) sức khỏe Covid 19")
