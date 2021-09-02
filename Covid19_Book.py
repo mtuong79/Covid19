c7b61d09a5408854266cd979945d498856aee920
mtuong79 <89337936+mtuong79@users.noreply.github.com>
2021-09-02 14:12:19 +0700

    Update Covid19_Book.py

--- Covid19_Book.py
+++ Covid19_Book.py
@@ -38,8 +38,9 @@ else:
 st.subheader("TÌM HIỂU THÊM")
 st.write("Cổng thông tin Covid19 [HCM](https://covid19.hochiminhcity.gov.vn/home)")
 st.write("[Sổ tay](https://covid19.hochiminhcity.gov.vn/cham-soc-suc-khoe/-/asset_publisher/ZSHLi888uq2s/content/so-tay-suc-khoe-covid--1?inheritRedirect=false&redirect=https%3A%2F%2Fcovid19.hochiminhcity.gov.vn%2Fcham-soc-suc-khoe%3Fp_p_id%3D101_INSTANCE_ZSHLi888uq2s%26p_p_lifecycle%3D0%26p_p_state%3Dnormal%26p_p_mode%3Dview%26p_p_col_id%3Dcolumn-10%26p_p_col_count%3D1) sức khỏe Covid 19")
-st.write("[Giúp Tôi](https://www.giuptoi.vn/) - Kết Nối và Trợ Giúp")
+st.write("[Túi thuốc](https://thanhnien.vn/suc-khoe/tui-thuoc-danh-cho-f0-dieu-tri-tai-nha-gom-nhung-loai-nao-1442437.html) dành cho F0 điều trị tại nhà")
 st.write("Bệnh viện Chợ Rẫy khám bệnh [online](https://thanhnien.vn/thoi-su/benh-vien-cho-ray-kham-benh-online-tu-9-gio-hom-nay-128-cho-cac-bn-khong-phai-benh-covid-19-1429436.html)")
+st.write("[Giúp Tôi](https://www.giuptoi.vn/) - Kết Nối và Trợ Giúp")
 st.write("Nhu yếu phẩm [cần hỗ trợ](https://thanhnien.vn/thoi-su/nguoi-dan-tphcm-ghi-nho-nhung-so-dien-thoai-nay-khi-can-ho-tro-nhu-yeu-pham-1434807.html)")
 st.write("Ứng dụng [An Sinh](https://vnexpress.net/ung-dung-giup-nguoi-dan-nhan-tui-an-sinh-tien-ho-tro-4350033.html) giúp người dân nhận túi an sinh, tiền hỗ trợ")
-st.write("[Túi thuốc](https://thanhnien.vn/suc-khoe/tui-thuoc-danh-cho-f0-dieu-tri-tai-nha-gom-nhung-loai-nao-1442437.html) dành cho F0 điều trị tại nhà")
+
